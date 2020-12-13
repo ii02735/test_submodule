cb1ec0f2e1116d440eafd3fa5bf8acd8699d5f1f
ii02735 <yadallee.bilaal@outlook.fr>
2020-12-13 12:48:07 +0100

    hello world module !

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const fs = require("fs")
 const path = require("path")
-app.get('/',(req,res) => res.json({"hello":"module !"}))
+app.get('/',(req,res) => res.json({"hello":"world module !"}))
 app.get('/build',(req,res) => {
     fs.readFile(path.join('test_submodule_module','build.json'),(err,data) => {
         if(!err)
